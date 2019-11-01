6811184c80b8c98f75bb45c17ca89acd317884ac
Akash Khatri <Akash Khatri@192.168.2.63>
2019-11-01 15:44:41 +0500

    First Commit

---
+++ AOP_Programatic_ARA/src/services/LogAfterService.java
@@ -0,0 +1,21 @@
+package services;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import bussiness.Bank;
+
+public class LogAfterService implements AfterReturningAdvice {
+
+	@Override
+	public void afterReturning(java.lang.Object arg0, Method arg1, java.lang.Object[] arg2, java.lang.Object arg3)
+			throws Throwable {
+		Log log = LogFactory.getLog(Bank.class);
+		log.info("deposit method..");
+	}
+	
+}
